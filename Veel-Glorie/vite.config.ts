66c3534f245285b936852ed21949ca99fc392e85
Lucas Vazquez <lucas.vazquez@enviame.io>
2023-07-21 02:15:58 -0300

    fix: test daisy variables

--- Veel-Glorie/vite.config.ts
+++ Veel-Glorie/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import jsonImporter from "node-sass-json-importer"
+import react from "@vitejs/plugin-react-swc"
 
 export default defineConfig({
   plugins: [react()],
@@ -7,5 +8,12 @@ export default defineConfig({
     alias: [
       { find: '@styles', replacement: './src/styles/base.scss' },
     ]
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        importer: jsonImporter(),
+      }
+    }
   },
 })
