a5553c35f0ba3d88e3fa3277c2ea99758bdbae7f
Lucas Vazquez <lucas.vazquez@enviame.io>
2023-08-16 03:24:20 -0300

    fix: fix types

--- Veel-Glorie/.d.ts
+++ Veel-Glorie/.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="vite/client" />
+
 declare module "node-sass-json-importer" {
   export default function (): () => any
 }
--- Veel-Glorie/vite.config.ts
+++ Veel-Glorie/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite"
 import jsonImporter from "node-sass-json-importer"
-import defaultEnv from "./default-env"
+import defaultEnv from "./default-env.js"
 import react from "@vitejs/plugin-react-swc"
 
 export default defineConfig({
