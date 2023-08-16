a4a4be4ca079ccfd31515b8499f6f0dedfc6eff2
Lucas Vazquez <lucas.vazquez@enviame.io>
2023-08-16 03:20:01 -0300

    fix: fix types

--- Veel-Glorie/.d.ts
+++ Veel-Glorie/.d.ts
@@ -2,40 +2,10 @@ declare module "node-sass-json-importer" {
   export default function (): () => any
 }
 
-declare module "default-env.js" {
-  export interface Dict {
-    READY: string,
-
-    SITE_DOMAIN: string,
-    SITE_NAME: string,
-    SITE_DESCRIPTION: string,
-    SITE_SLOGAN: string,
-
-    SCREEN_SM: string,
-    SCREEN_MD: string,
-    SCREEN_LG: string,
-    SCREEN_XL: string,
-    SCREEN_2XL: string,
-
-    COLOR_INFO: string,
-    COLOR_SUCCESS: string,
-    COLOR_WARNING: string,
-    COLOR_ERROR: string,
-    COLOR_PRIMARY: string,
-    COLOR_PRIMARY_FOCUS: string,
-    COLOR_PRIMARY_CONTENT: string,
-    COLOR_SECONDARY: string,
-    COLOR_SECONDARY_FOCUS: string,
-    COLOR_SECONDARY_CONTENT: string,
-    COLOR_ACENT: string,
-    COLOR_ACENT_FOCUS: string,
-    COLOR_ACENT_CONTENT: string,
-    COLOR_NEUTRAL: string,
-    COLOR_NEUTRAL_FOCUS: string,
-    COLOR_NEUTRAL_CONTENT: string,
-    COLOR_BASE_100: string,
-    COLOR_BASE_200: string,
-    COLOR_BASE_300: string,
-    COLOR_BASE_CONTENT: string,
+// default.env.js
+declare module "*.js" {
+  interface DefaultEnv {
+    [key: string]: string
   }
+  export default object as DefaultEnv
 }
