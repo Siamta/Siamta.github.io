6d24729861b38b18f9229d00f161fff73095b5e6
Lucas Vazquez <lucas5zvazquez@gmail.com>
2024-09-24 09:24:13 -0300

    feat: update introduction and simplify styles

--- Veel-Glorie/src/components/YouTube/YouTube.tsx
+++ Veel-Glorie/src/components/YouTube/YouTube.tsx
@@ -8,11 +8,7 @@ function YouTube({ title, url }: YouTubeProps): JSX.Element {
     <iframe
         src={`https://www.youtube.com/embed/${url}`}
         title={`${title} - [YouTube]`}
-        className="
-          border-none
-          w-[17rem] sm:w-[26rem]  md:w-[40rem]
-          h-[8rem]  sm:h-[14rem]  md:h-[23rem]
-        "
+        className="border-none w-[17rem] sm:w-[41rem] h-[8rem] sm:h-[23rem]"
         allow="accelerometer; autoplay"
         referrerPolicy="strict-origin-when-cross-origin"
         allowFullScreen
--- Veel-Glorie/src/layout/Introduction/Introduction.tsx
+++ Veel-Glorie/src/layout/Introduction/Introduction.tsx
@@ -9,22 +9,30 @@ function Introduction(): JSX.Element {
       <div className="flex border-4 rounded-md border-primary border-color-primary border-[primary]">
         <YouTube title="Video gremial" url="ix-pyuUBMlE?si=h_nDnFwrtVFoUI3O" />
       </div>
-      <div className="flex flex-col space-y-1 text-center px-0 md:px-8">
+      <div className="flex flex-col text-center">
         <p>
           <i>
             <b>{process.env.SITE_NAME}</b> significa <i>Mucha Gloria</i>,
             y la sangre de nuestros soldados es un reflejo de la lucha y los actos gloriosos en sí mismos.
+            Más que un gremio, a veces parecemos una comunidad o un grupo de amigos.
+            Llevamos un buen tiempo juntos y entre todos defendemos nuestros ideales y tomamos las decisiones de manera conjunta.
           </i>
         </p>
         <p>
           <i>
             En nuestro corazón reside la lucha,
             pero de todas maneras realizamos todo tipo de actividades.
+            Pasarla bien no es opción, ser tóxicos tampoco.
+            Pelear es un deber, pero divertirse es obligación.
+            Esperamos que todos sepan pelear,
+            pero que también disfruten su contenido individual y hagan su propia economía.
           </i>
         </p>
         <p>
           <i>
             Nuestra misión es convertir a granjeros en verdaderas herramientas bélicas.
+            A través de la capacitación, las peleas y las prácticas,
+            buscamos desatar el potencial que reside en cada persona.
           </i>
         </p>
       </div>
