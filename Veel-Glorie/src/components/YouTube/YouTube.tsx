666d2db66a058c023d701bf4809ac15c765dc053
Lucas Vazquez <lucas5zvazquez@gmail.com>
2024-09-24 09:53:57 -0300

    feat: improve YT + iframe parameters

--- Veel-Glorie/src/components/YouTube/YouTube.tsx
+++ Veel-Glorie/src/components/YouTube/YouTube.tsx
@@ -6,12 +6,15 @@ interface YouTubeProps {
 function YouTube({ title, url }: YouTubeProps): JSX.Element {
   return (
     <iframe
-        src={`https://www.youtube.com/embed/${url}`}
-        title={`${title} - [YouTube]`}
-        className="border-none w-[17rem] sm:w-[41rem] h-[8rem] sm:h-[23rem]"
-        allow="accelerometer; autoplay"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
+      // Documentation about parameters:
+      // - controls: https://developers.google.com/youtube/player_parameters#Parameters
+      // - allow: https://developer.mozilla.org/en-US/docs/Web/HTTP/Permissions_Policy#browser_compatibility
+      src={`https://www.youtube.com/embed/${url}?controls=2&iv_load_policy=3&cc_load_policy=0&origin=*&rel=0`}
+      title={`${title} - YouTube`}
+      className="border-none w-[17rem] sm:w-[41rem] h-[8rem] sm:h-[23rem]"
+      allow="accelerometer; encrypted-media; fullscreen; geolocation; gyroscope; picture-in-picture; web-share"
+      referrerPolicy="strict-origin-when-cross-origin"
+      allowFullScreen
     />
   )
 }
--- Veel-Glorie/src/layout/Introduction/Introduction.tsx
+++ Veel-Glorie/src/layout/Introduction/Introduction.tsx
@@ -9,31 +9,28 @@ function Introduction(): JSX.Element {
       <div className="flex border-4 rounded-md border-primary border-color-primary border-[primary]">
         <YouTube title="Video gremial" url="ix-pyuUBMlE?si=h_nDnFwrtVFoUI3O" />
       </div>
-      <div className="flex flex-col text-center">
-        <p>
-          <i>
-            <b>{process.env.SITE_NAME}</b> significa <i>Mucha Gloria</i>,
-            y la sangre de nuestros soldados es un reflejo de la lucha y los actos gloriosos en sí mismos.
-            Más que un gremio, a veces parecemos una comunidad o un grupo de amigos.
-            Llevamos un buen tiempo juntos y entre todos defendemos nuestros ideales y tomamos las decisiones de manera conjunta.
-          </i>
+      <p className="text-center">
+        <b>{process.env.SITE_NAME}</b> significa <i>Mucha Gloria</i>,
+        y la sangre de nuestros soldados es un reflejo de la lucha y los actos gloriosos en sí mismos.
+      </p>
+      <div className="flex flex-col pt-4">
+        </p>
+          Más que un gremio, a veces parecemos una comunidad o un grupo de amigos.
+          Llevamos un buen tiempo juntos y, entre todos,
+          defendemos nuestros ideales, nuestro patrimonio y tomamos las decisiones de manera conjunta.
         </p>
         <p>
-          <i>
-            En nuestro corazón reside la lucha,
-            pero de todas maneras realizamos todo tipo de actividades.
-            Pasarla bien no es opción, ser tóxicos tampoco.
-            Pelear es un deber, pero divertirse es obligación.
-            Esperamos que todos sepan pelear,
-            pero que también disfruten su contenido individual y hagan su propia economía.
-          </i>
+          En nuestro corazón reside la lucha,
+          pero de todas maneras realizamos todo tipo de actividades.
+          Pasarla bien no es opción, ser tóxicos tampoco.
+          Pelear es un deber, pero divertirse es obligación.
+          Esperamos que todos sepan pelear,
+          pero que también disfruten su contenido individual y hagan su propia economía.
         </p>
         <p>
-          <i>
-            Nuestra misión es convertir a granjeros en verdaderas herramientas bélicas.
-            A través de la capacitación, las peleas y las prácticas,
-            buscamos desatar el potencial que reside en cada persona.
-          </i>
+          Nuestra misión es convertir a granjeros en verdaderas herramientas bélicas.
+          A través de la capacitación, las peleas y las prácticas,
+          buscamos desatar el potencial que reside en cada persona.
         </p>
       </div>
     </Section>
