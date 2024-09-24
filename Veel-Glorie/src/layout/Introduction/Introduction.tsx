454f367c9f45c206d109e9d890a15a635f9c091e
Lucas Vazquez <lucas5zvazquez@gmail.com>
2024-09-24 07:58:40 -0300

    feat: add border to YT introduction and remove image

--- Veel-Glorie/src/layout/Introduction/Introduction.tsx
+++ Veel-Glorie/src/layout/Introduction/Introduction.tsx
@@ -4,28 +4,21 @@ import YouTube from "../../components/YouTube"
 function Introduction(): JSX.Element {
   return (
     <Section transparency background="secondary" separation={separations.BIG_DOUBLE}>
-      <div className="flex">
+      <div className="flex border-4 rounded-md border-primary border-color-primary border-[primary] border-color-primary border-red">
         <YouTube title="Video gremial" url="ix-pyuUBMlE?si=h_nDnFwrtVFoUI3O" />
       </div>
-      <div className="flex justify-center">
-        <div className="avatar">
-          <div className="mask mask-squircle w-56 h-56 md:w-80 md:h-80">
-            <img src="/images/people.png?version=postprocess_VERSION" className="max-w-full min-h-full" />
-          </div>
-        </div>
-        <div className="flex flex-col space-y-8 m-auto max-w-prose px-8 md:px-0 text-center md:text-start">
-          <p>
-            <b>{process.env.SITE_NAME}</b> significa <i>Mucha Gloria</i>,
-            y la sangre de nuestros soldados es un reflejo de la lucha y los actos gloriosos en sí mismos.
-          </p>
-          <p>
-            En nuestro corazón reside la lucha,
-            pero de todas maneras realizamos todo tipo de actividades.
-          </p>
-          <p>
-            Nuestra misión es convertir a granjeros en verdaderas herramientas bélicas.
-          </p>
-        </div>
+      <div className="flex flex-col text-center max-w[35rem]">
+        <p>
+          <b>{process.env.SITE_NAME}</b> significa <i>Mucha Gloria</i>,
+          y la sangre de nuestros soldados es un reflejo de la lucha y los actos gloriosos en sí mismos.
+        </p>
+        <p>
+          En nuestro corazón reside la lucha,
+          pero de todas maneras realizamos todo tipo de actividades.
+        </p>
+        <p>
+          Nuestra misión es convertir a granjeros en verdaderas herramientas bélicas.
+        </p>
       </div>
     </Section>
   )
